f8883223fe1cd4f382596298a2825165f78a0411
loveloli <loveloli@MSI>
2020-07-31 09:26:20 +0800

    number v1.1

--- 200731/src/com/zhaiyu/test/Test1.java
+++ 200731/src/com/zhaiyu/test/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("holleworld");
+
+
+int a =1111;
+
+System.out.println(a);
 	}
 
 }
